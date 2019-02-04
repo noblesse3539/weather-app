81092256cb2f8dfb2763ea211e47817b8399de26
opalcat <noblesse3539@daum.net>
2019-02-04 23:24:36 +0900

    Getting the Weather

--- App.js
+++ App.js
@@ -1,28 +1,41 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View, StatusBar } from 'react-native';
-import Weather from './Weather'
+import Weather from './Weather';
 
+
+const API_KEY= "";
 export default class App extends React.Component {
   state = {
     isLoaded: false,
-    error: null
+    error: null,
+    temperature: null,
+    name: null
   }
   componentDidMount() {
     navigator.geolocation.getCurrentPosition( 
       position => {
-        this.setState( {
-          isLoaded: true
-        });
+        this._getWeather(position.coords.latitude, position.coords.longitude);
       },
       error => {
         this.setState( {
           error: error
-        })
+        });
       }
     );
   }
+  _getWeather = (lat, long) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&APPID=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        temperature: json.main.temp,
+        name:json.weather[0].main,
+        isLoaded: true
+      })
+    });
+  }
   render() {
-    const { isLoaded } = this.state;
+    const { isLoaded, error } = this.state;
     return (
       <View style={styles.container}>
         <StatusBar hidden={true}  />
